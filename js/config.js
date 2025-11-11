f3a244a130591875306049397e4bec23cde0633f
jdkys1 <162021746+jdkys1@users.noreply.github.com>
2025-11-11 19:53:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -23,9 +23,9 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {
     dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod/',
+        api: 'http://dyttzy1.tv/api.php/provide/vod',
         name: '电影天堂资源',
-        detail: 'http://caiji.dyttzyapi.com', 
+        detail: 'http://dyttzy1.tv', 
     },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
